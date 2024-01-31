1fbe33cd3a99b56bf16b2120f73418c614b31415
wandecharles <125980401+wandecharles@users.noreply.github.com>
2024-01-31 17:29:52 -0600

    Add files via upload
    Problem Statement - Honey Production Case Study-2
    Background: In 2006, global concern was raised over the rapid decline in the honeybee population, an integral component of American honey agriculture. Large numbers of hives were lost to Colony Collapse Disorder, a phenomenon of disappearing worker bees causing the remaining hive colony to collapse. Speculation to the cause of this disorder points to hive diseases and pesticides harming the pollinators, though no overall consensus has been reached. The U.S. used to locally produce over half the honey it consumes per year. Now, honey mostly comes from overseas, with 350 of the 400 million pounds of honey consumed every year originating from imports. This dataset provides insights into honey production supply and demand in America from 1998 to 2016.
    Objective: To visualize how honey production has changed over the years (1998-2016) in the United States. 
    Key questions to be answered:
    •	How has honey production yield changed from 1998 to 2016?
    •	Over time, what are the major production trends across the states?
    •	Are there any patterns that can be observed between total honey production and the value of production every year? How has value of production, which in some sense could be tied to demand, changed every year?
    Dataset:
    •	state: Various states of the U.S.
    •	year: Year of production
    •	stocks: Refers to stocks held by producers. Unit is pounds
    •	numcol: Number of honey-producing colonies. Honey producing colonies are the maximum number of colonies from which honey was taken during the year. It is possible to take honey from colonies that did not survive the entire year
    •	yieldpercol: Honey yield per colony. Unit is pounds
    •	totalprod: Total production (numcol x yieldpercol). Unit is pounds
    •	priceperlb: Refers to the average price per pound based on expanded sales. The unit is dollars.
    •	prodvalue: Value of production (totalprod x priceperlb). The unit is dollars.
    Other useful information: Certain states are excluded every year (ex. CT) to avoid disclosing data for individual operations. Due to rounding, total colonies multiplied by total yield may not equal production. Also, the summation of states will not equal the U.S. level value of production.

---
+++ honey production insights.sql
@@ -0,0 +1,78 @@
+create table honeyprod (state varchar(100),
+						numcol double precision,
+						yieldpercol int,
+						totalprod double precision,
+						stocks text,
+						priceperlb money,
+						prodvalue numeric,
+						year int)
+
+drop table honeyprod
+select * from honeyprod
+
+-----------How has honey production yield changed from 1998 to 2016?
+select year, sum(totalprod) from honeyprod
+group by year
+order by year asc
+
+------------Over time, what are the major production trends across the states?
+select state, sum(totalprod) from honeyprod
+group by state 
+order by state
+
+select state, sum(prodvalue) from honeyprod
+group by state 
+order by state;
+
+-----price per pound for every year for every state
+select state, year, priceperlb from honeyprod 
+order by state, year
+
+
+-----state with the highest number of honey producing colonies(north dakota)
+select state, sum(numcol) from honeyprod
+group by state 
+order by sum(numcol) desc
+limit 1
+
+--------state with the lowest number of honey producing colonies("Oklahoma")
+select state, sum(numcol) from honeyprod
+group by state 
+order by sum(numcol) asc
+limit 1
+
+-------yeild per colony for north dakota
+select year, yieldpercol from honeyprod
+where state ilike 'north dakota%'
+order by year
+
+-----prodcution value of north dakota per year
+select year, prodvalue from honeyprod
+where state ilike 'north dakota%'
+order by year
+
+----------yeild per colony for Oklahoma
+select year, yieldpercol from honeyprod
+where state ilike 'Oklahoma%'
+order by year
+
+-----prodcution value of Oklahoma per year
+select year, prodvalue from honeyprod
+where state ilike 'Oklahoma%'
+order by year
+
+
+
+-----------Are there any patterns that can be observed between total honey production and the value of production every year? 
+--How has value of production, which in some sense could be tied to demand, changed every year?
+select year, sum(totalprod) as total_honey_production, sum(prodvalue) as value_of_production from honeyprod
+group by year
+order by year
+
+
+
+
+
+
+state,  numcol  ,yieldpercol, totalprod  ,stocks,  priceperlb,    prodvalue,   year
+Alabama,  16000  ,71,         1136000  ,159000,  0.72,             818000,     1998
